a806ec61e715ee9a556ebeafdfb7c9b3f140146c
Matheus Castro <mate.cas37@gmail.com>
2023-12-15 16:19:26 -0300

    feat: adicionando server fastify.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world!')
+import fastify from "fastify";
+import "dotenv/config";
+
+const server = fastify();
+
+const portDotEnv = process.env.PORT;
+
+const port = portDotEnv as number | undefined;
+
+server.listen({ port: port }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
+
+server.get("/", async (req, res) => console.log("hello world"));
