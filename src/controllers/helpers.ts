5a5a73a162b41a7ce1b9aa8800806424dade4fb7
Matheus Castro <mate.cas37@gmail.com>
2023-12-18 12:19:55 -0300

    refactor: adicionando HttpStatusCode enum

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -1,22 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { HttpResponse } from "./protocols";
+import { HttpResponse, HttpStatusCode } from "./protocols";
 
 export const ok = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 200,
+  statusCode: HttpStatusCode.OK,
   body,
 });
 
 export const created = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 201,
+  statusCode: HttpStatusCode.CREATED,
   body,
 });
 
 export const badRequest = (message: string): HttpResponse<string> => ({
-  statusCode: 200,
+  statusCode: HttpStatusCode.BAD_REQUEST,
   body: message,
 });
 
 export const serverError = (): HttpResponse<string> => ({
-  statusCode: 200,
+  statusCode: HttpStatusCode.SERVER_ERROR,
   body: "Algo deu errado.",
 });
--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface HttpResponse<T> {
-  statusCode: number;
+  statusCode: HttpStatusCode;
   body: T | string;
 }
 
@@ -8,6 +8,13 @@ export interface HttpRequest<B> {
   headers?: any;
   params?: any;
   body?: B;
+}
+
+export enum HttpStatusCode {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  SERVER_ERROR = 500,
 }
 
 export interface IController {
